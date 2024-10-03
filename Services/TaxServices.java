f0ff6ba7503bdbb1592720b6b0c8bda3c52e5a6d
Raphael Magno <150822511+RMagnoRJ@users.noreply.github.com>
2024-10-03 13:07:43 -0300

    Inseri comentário sobre o uso da INTERFACE em POO

--- Services/TaxServices.java
+++ Services/TaxServices.java
@@ -4,4 +4,22 @@ public interface TaxServices {
     
     public Double addBasicPayment(Double valor);
 
+    /*
+        * ================================================================
+        * 
+        *                             INTERFACE
+        * 
+        * ================================================================
+        * 
+        * A interface é um tipo que define um conjunto de operações que uma 
+        * classe deve implementar. A interface estabelece um CONTRATO que
+        * a classe deve cumprir.
+        * 
+        * É uma classe ABSTRATA que possui APENAS MÉTODOS e todos os seus
+        * métodos TAMBÉM SÃO ABSTRATOS.
+        * 
+        * 
+        * 
+    */
+
 }
